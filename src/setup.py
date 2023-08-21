8ee5013a0ef6be63fd59a3c9d5b0067179829371
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2023-08-21 11:42:09 +0200

    change the make_folder function

--- src/setup.py
+++ src/setup.py
@@ -8,45 +8,48 @@
 
 from pathlib import Path
 
-# 01 - setup working environment
-# --------------------------------
+def make_folders():
+    # 01 - setup working environment
+    # --------------------------------
 
-# create directory called 'data' with subdirectories
-# 'raw_data', 'processed_data', and 'metadata'
-base_dir = Path(__file__).parents[1]
+    # create directory called 'data' with subdirectories
+    # 'raw_data', 'processed_data', and 'metadata'
+    base_dir = Path.cwd().parent
 
-data_directories = [
-    base_dir / 'data' / 'raw_data' / 'nDSM',
-    base_dir / 'data' / 'raw_data' / 'DOP',
-    base_dir / 'data' / 'raw_data' / 'gaps_poly',
-    base_dir / 'data' / 'processed_data',
-    base_dir / 'data' / 'metadata'
-]
+    data_directories = [
+        base_dir / 'data' / 'raw_data' / 'nDSM',
+        base_dir / 'data' / 'raw_data' / 'DOP',
+        base_dir / 'data' / 'raw_data' / 'gaps_poly',
+        base_dir / 'data' / 'processed_data',
+        base_dir / 'data' / 'metadata'
+    ]
 
-for directory in data_directories:
-    directory.mkdir(parents=True, exist_ok=True)
+    for directory in data_directories:
+        directory.mkdir(parents=True, exist_ok=True)
+        print(f'created directory {directory.relative_to(base_dir)}')
 
-# create other necessary directories
-other_directories = [
-    base_dir / 'src',
-    base_dir / 'scripts',
-    base_dir / 'output'
-]
 
-for directory in other_directories:
-    directory.mkdir(parents=True, exist_ok=True)
+    # create other necessary directories
+    other_directories = [
+        base_dir / 'output'
+    ]
 
-# 02 - file path definitions
-# ---------------------------
+    for directory in other_directories:
+        directory.mkdir(parents=True, exist_ok=True)
+        print(f'created directory {directory.relative_to(base_dir)}')
 
-# define raw data directory
-raw_data_dir = base_dir / 'data' / 'raw_data'
+    # 02 - file path definitions
+    # ---------------------------
 
-# define processed data directory
-processed_data_dir = base_dir / 'data' / 'processed_data'
+    # define raw data directory
+    raw_data_dir = base_dir / 'data' / 'raw_data'
 
-# define output directory
-output_dir = base_dir / 'output'
+    # define processed data directory
+    processed_data_dir = base_dir / 'data' / 'processed_data'
 
-# list the files and directories
-print([p.name for p in base_dir.iterdir()])
+    # define output directory
+    output_dir = base_dir / 'output'
+
+
+
+    return raw_data_dir, processed_data_dir, output_dir
