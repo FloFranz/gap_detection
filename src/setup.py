b2663d67d6d2f706117d7839bb64a7fb692528f3
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2023-08-21 11:27:16 +0200

    change to pathlib

--- src/setup.py
+++ src/setup.py
@@ -6,52 +6,47 @@
 # Contact:      florian.franz@nw-fva.de
 #-------------------------------------------------------------
 
-import os
+from pathlib import Path
 
 # 01 - setup working environment
 # --------------------------------
 
 # create directory called 'data' with subdirectories
 # 'raw_data', 'processed_data', and 'metadata'
-base_dir = os.path.abspath('.')[:-8]
+base_dir = Path(__file__).parents[1]
 
 data_directories = [
-    os.path.join(base_dir, 'data/raw_data/nDSM'),
-    os.path.join(base_dir, 'data/raw_data/DOP'),
-    os.path.join(base_dir, 'data/raw_data/gaps_poly'),
-    os.path.join(base_dir, 'data/processed_data'),
-    os.path.join(base_dir, 'data/metadata')
+    base_dir / 'data' / 'raw_data' / 'nDSM',
+    base_dir / 'data' / 'raw_data' / 'DOP',
+    base_dir / 'data' / 'raw_data' / 'gaps_poly',
+    base_dir / 'data' / 'processed_data',
+    base_dir / 'data' / 'metadata'
 ]
 
 for directory in data_directories:
-    if not os.path.exists(directory):
-        os.makedirs(directory)
+    directory.mkdir(parents=True, exist_ok=True)
 
 # create other necessary directories
 other_directories = [
-    os.path.join(base_dir, 'src'),
-    os.path.join(base_dir, 'scripts'),
-    os.path.join(base_dir, 'output')
- ]
+    base_dir / 'src',
+    base_dir / 'scripts',
+    base_dir / 'output'
+]
 
 for directory in other_directories:
-    if not os.path.exists(directory):
-        os.makedirs(directory)
+    directory.mkdir(parents=True, exist_ok=True)
 
 # 02 - file path definitions
 # ---------------------------
 
 # define raw data directory
-raw_data_dir = 'data/raw_data/'
-raw_data_dir = os.path.join(base_dir, raw_data_dir)
+raw_data_dir = base_dir / 'data' / 'raw_data'
 
 # define processed data directory
-processed_data_dir = 'data/processed_data/'
-processed_data_dir = os.path.join(base_dir, processed_data_dir)
+processed_data_dir = base_dir / 'data' / 'processed_data'
 
 # define output directory
-output_dir = 'output/'
-output_dir = os.path.join(base_dir, output_dir)
+output_dir = base_dir / 'output'
 
 # list the files and directories
-print(os.listdir(base_dir))
+print([p.name for p in base_dir.iterdir()])
