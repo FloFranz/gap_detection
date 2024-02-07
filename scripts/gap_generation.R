fbcd4b5f4e49595f5fb6ff6bdd3f2f66de7f6d3e
FloFranz <florian.franz86@gmx.de>
2024-02-07 09:19:10 +0100

    generate gap polygons for testing

--- scripts/gap_generation.R
+++ scripts/gap_generation.R
@@ -1,7 +1,7 @@
 #----------------------------------------------------------------------------
 # Name:         gap_generation.R
 # Description:  Script automatically generates canopy gaps as
-#               training polygons for the deep learning model.
+#               training and testing polygons for the deep learning model.
 #               For this purpose, the ForestGapR package is applied 
 #               to an airborne laserscanning (ALS)-based canopy 
 #               height model (CHM) in 0.5 m resolution.
@@ -9,7 +9,6 @@
 #               Silva et al. 2019,  https://doi.org/10.1111/2041-210X.13211
 # Contact:      florian.franz@nw-fva.de
 #----------------------------------------------------------------------------
-
 
 
 
@@ -45,22 +44,39 @@ nlf_org
 # assign CRS to CHM (ETRS89 / UTM zone 32N)
 terra::crs(chm_solling) <- 'EPSG:25832'
 
-# select forestry district 'Hilwartshausen'
-revier <- nlf_org[nlf_org$REVIERNAME == 'Hilwartshausen',]
+# select forestry districts 
+# 'Hilwartshausen' and 'Fredelsloh'
+revier_hilwartshausen <- nlf_org[nlf_org$REVIERNAME == 'Hilwartshausen',]
+revier_fredelsloh <- nlf_org[nlf_org$REVIERNAME == 'Fredelsloh',]
 
-# crop CHM to selected forestry district
-chm_revier <- terra::crop(chm_solling, revier, mask = T)
+# crop CHM to selected forestry districts
+chm_revier_hilwartshausen <- terra::crop(chm_solling, 
+                                         revier_hilwartshausen,
+                                         mask = T)
+chm_revier_fredelsloh <- terra::crop(chm_solling, 
+                                     revier_fredelsloh,
+                                     mask = T)
 
 # smooth cropped CHM
 w <- matrix(1, 3, 3)
-chm_revier <- terra::focal(chm_revier, w, fun = mean, na.rm = T)
+chm_revier_hilwartshausen <- terra::focal(chm_revier_hilwartshausen,
+                                          w, fun = mean, na.rm = T)
+chm_revier_fredelsloh <- terra::focal(chm_revier_fredelsloh,
+                                      w, fun = mean, na.rm = T)
 
 # quick overview
 par_org <- par()
 par(mfrow = c(1,2))
 terra::plot(chm_solling, col = viridis::viridis(50))
-terra::plot(revier$geometry, border = 'white', lwd = 2, add = T)
-terra::plot(chm_revier, col = viridis::viridis(50))
+terra::plot(revier_hilwartshausen$geometry, border = 'white', lwd = 2, add = T)
+terra::plot(chm_revier_hilwartshausen, col = viridis::viridis(50))
+par(par_org)
+
+par_org <- par()
+par(mfrow = c(1,2))
+terra::plot(chm_solling, col = viridis::viridis(50))
+terra::plot(revier_fredelsloh$geometry, border = 'white', lwd = 2, add = T)
+terra::plot(chm_revier_fredelsloh, col = viridis::viridis(50))
 par(par_org)
 
 
@@ -71,7 +87,7 @@ par(par_org)
 # determine overstory height
 # assuming overstory height is the
 # 95th percentile of the height values
-overstory_height <- stats::quantile(chm_revier,
+overstory_height <- stats::quantile(chm_revier_fredelsloh,
                                     probs = 0.95,
                                     na.rm = T)
 
@@ -86,7 +102,7 @@ half_overstory_height <- overstory_height / 2
 # --> https://github.com/carlos-alberto-silva/ForestGapR
 # min. size 10m², max. size 5000m²
 canopy_gaps <- ForestGapR::getForestGaps(
-  chm_layer = chm_revier,
+  chm_layer = chm_revier_fredelsloh,
   threshold = half_overstory_height,
   size = c(10,5000)
   )
@@ -104,23 +120,23 @@ canopy_gaps <- sf::st_as_sf(canopy_gaps)
 
 # write to disk
 if (!file.exists(file.path(
-  processed_data_dir, 'gap_polygons', 'gap_polys_hilwartshausen.shp'))) {
+  processed_data_dir, 'gap_polygons', 'gap_polys_fredelsloh.shp'))) {
  
   dir.create(file.path(processed_data_dir, 'gap_polygons'), recursive = T)
   sf::st_write(canopy_gaps,
                file.path(processed_data_dir,
                          'gap_polygons',
-                         'gap_polys_hilwartshausen.shp'))
+                         'gap_polys_fredelsloh.shp'))
   
 } else {
     
   canopy_gaps <- sf::st_read(file.path(
-    processed_data_dir, 'gap_polygons', 'gap_polys_hilwartshausen.shp'))
+    processed_data_dir, 'gap_polygons', 'gap_polys_fredelsloh.shp'))
   
 }
 
 # quick overview
-terra::plot(chm_revier, col = viridis::viridis(50))
+terra::plot(chm_revier_fredelsloh, col = viridis::viridis(50))
 terra::plot(canopy_gaps$geometry,
             border = 'red',
             lwd = 0.5,
